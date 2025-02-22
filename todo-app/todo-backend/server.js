4fd20c4c805b45babb01d73cbdcad638ea162eb1
Spriha <shringispriha@gmail.com>
2025-02-22 23:13:47 +0530

    deploy

--- todo-app/todo-backend/server.js
+++ todo-app/todo-backend/server.js
@@ -5,10 +5,21 @@ const connectDB = require('./config/db');
 
 const app = express();
 
+const corsOptions = {
+    origin: ["https://todo-application-hunger-box.vercel.app"], // Allow frontend
+    methods: "GET,POST,PUT,DELETE", // Allowed HTTP methods
+    allowedHeaders: "Content-Type,Authorization", // Allowed headers
+    credentials: true, // Allow cookies if needed
+  };
+  
+  app.use(cors(corsOptions));
+  
 // Middleware
 app.use(express.json());
 // app.use(cors());
-app.use(cors({ origin: "https://todo-application-hunger-box.vercel.app" }));
+// app.use(cors({ origin: "https://todo-application-hunger-box.vercel.app"}));
+app.options('*', cors(corsOptions));
+
 // Connect to Database
 connectDB();
 
